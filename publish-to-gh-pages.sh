df7988b5140313bfc6521dfad680cbbf6d45bc74
Hao Chen <hao.chen@vector.co.nz>
2018-06-11 21:02:00 +1200

    Initial commit

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+
+cd ../
+mv .deploy_git/.git/ ./public/
+
+cd ./public
+
+git config user.name "Hao Chen"
+git config user.email "hao.ch86@gmail.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
+
+# Coding Pages
+git push --force --quiet "https://Leafney:${CodingToken}@${CD_REF}" master:master
